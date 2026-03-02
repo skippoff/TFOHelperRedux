47ccb84b8ba5978d5aa377b859c19a1b9a8b4e7f
Skipoff <miha.skip@gmail.com>
2026-03-02 18:18:37 +0200

    feat: Улучшена читаемость текста в карточках точек лова

---
+++ TFOHelperRedux/Converters/StripPrefixConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TFOHelperRedux.Converters;
+
+/// <summary>
+/// Удаляет префикс из строки (всё до ": " включительно)
+/// Например: "Особенности: трофейная" → "трофейная"
+/// </summary>
+public class StripPrefixConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not string str)
+            return value;
+
+        var idx = str.IndexOf(": ", StringComparison.Ordinal);
+        return idx >= 0 ? str.Substring(idx + 2) : str;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
